4a62d149e71f5c1cdf6ccca90434000bc9ed61dc
bdx_inc <bdxinc.contact@gmail.com>
2018-01-02 00:56:27 -0800

    more tinkering
    more tinkering

--- bdx_projects_microsite.js
+++ bdx_projects_microsite.js
@@ -1,3 +1,20 @@
 (function(window, document, undefined) {
   // js nonsense
+  function handleClick(event) {
+    if (event.target !== event.currentTarget && event.target.nodeName === "BUTTON") {
+      console.log(event.target);
+
+      dropMenu(event.target);
+    }
+    event.stopPropagation();
+  }
+
+  var buttonEl = document.getElementById("menuBtn");
+  buttonEl.addEventListener("click", handleClick, true);
+
+  function dropMenu(clicked) {
+    // js or css stuff should happen
+    console.log(clicked);
+  }
+
 })(window = window || {}, document)
